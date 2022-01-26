0901ac0cbddcaca8854a662fe3be3f67cb86905a
jocko981 <jovannikolic17@gmail.com>
2022-01-26 18:44:35 +0100

    added page components into routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 // styles
 import "./App.css";
+// pages
+import Dashboard from "./pages/dashboard/Dashboard";
+import CreateDeveloper from "./pages/createDeveloper/CreateDeveloper";
+import DeveloperProfile from "./pages/developerProfile/DeveloperProfile";
+import EditDeveloper from "./pages/editDeveloper/EditDeveloper";
 
 function App() {
   return (
@@ -10,11 +15,12 @@ function App() {
         <div className="container">
           {/* <Navbar /> */}
           <Routes>
-            <Route path="/" element={<h1>Dashboard</h1>} />
-            <Route path="/create-developer" element={<h1>create profile</h1>} />
-            <Route path="/developers/:id" element={<h1>developers profile</h1>} />
-            <Route path="/hire-record" element={<h1>create profile</h1>} />
-            <Route path="/records/:id" element={<h1>project</h1>} />
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/create-developer" element={<CreateDeveloper />} />
+            <Route path="/developers/edit/:id" element={<EditDeveloper />} />
+            <Route path="/developers/:id" element={<DeveloperProfile />} />
+            <Route path="/hire-record" element={<h1>hire-record</h1>} />
+            <Route path="/records/:id" element={<h1>record</h1>} />
             <Route path="*" element={<h1>Error page</h1>} />
           </Routes>
         </div>
